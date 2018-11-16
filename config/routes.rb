f0237012cb41068f54bd4bd2c9fce0087d525cf4
Eike Feld <eike.feld@gmx.de>
2018-11-16 12:53:39 +0100

    Cocktail Master

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+  resources :doses, only: :destroy
 end
